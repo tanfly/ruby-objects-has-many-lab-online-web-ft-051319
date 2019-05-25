c004ebcbd3243b90d7de62a239794c3df40a854d
Stella McEwen <mcewen.stella@gmail.com>
2019-05-25 01:04:19 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,9 +7,9 @@ class Author
     @posts = []
   end
   
-  def add_post(title) 
-    @posts << title
-    title.author = self
+   def add_post(post)
+    @posts << post
+    post.author = self
   end
   
   def add_post_by_title(title)
